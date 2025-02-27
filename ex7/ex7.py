e6e95305d1ca7689ff891b6ae03e78acd47ff5e9
Matheus Bueno de Oliveira <135169698+MatheusBuenodeOliveira@users.noreply.github.com>
2025-02-27 01:12:42 +0000

    ex7

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -1,19 +1,23 @@
 #A partir de um vetor de números inteiros, calcule a soma e o produto dos elementos do vetor.
 
 def ex7(n):
-    if n.length() == 0:
-        soma = n[0]
-        produto = n[0]
-        return soma,produto
+    if len(n) == 0:
+        return 0, 1
+
+    soma_restante, produto_restante = ex7(n[1:])
+
+    soma = n[0] + soma_restante
     
-    return 
+    produto = n[0] * produto_restante
+
+    return soma, produto
  
 
 def main():
     
     n = [1,2,3,4]  
     result,result2 = ex7(n)
-    print(f"The result for n={n} is: {result}")
+    print(f"The result for n={n} is: {result,result2}")
 
 if __name__ == "__main__":
     main()
