05206dae7fb8bc8a73c40b92bdacf38e3a1685ac
João Pedro da Silva Kriger <128426379+sillysecret@users.noreply.github.com>
2025-02-27 00:55:57 +0000

    feat: ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write(ReverseString("joao"));
+
+
+string ReverseString(string str)
+{
+    if (str.Length <= 1)
+    {
+        return str;
+    }
+    
+    return str[str.Length - 1] + ReverseString(str.Substring(0, str.Length - 1));
+}
